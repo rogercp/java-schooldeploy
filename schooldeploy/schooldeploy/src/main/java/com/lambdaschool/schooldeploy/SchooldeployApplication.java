3656bd960e40b987153713bce0092e6786bdfd8c
Roger Caballero <rogercp1234@yahoo.com>
2019-06-19 14:14:19 -0700

    initial commit

---
+++ schooldeploy/schooldeploy/src/main/java/com/lambdaschool/schooldeploy/SchooldeployApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.schooldeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooldeployApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SchooldeployApplication.class, args);
+    }
+
+}
