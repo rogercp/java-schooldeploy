3094f11fd4c2cd4e910c6ca7db7f912342fd3238
Roger Caballero <rogercp1234@yahoo.com>
2019-06-19 17:40:41 -0700

    jar file deployment successful with enpoints and authentication working

--- schooldeploy/schooldeploy/src/main/java/com/lambdaschool/schooldeploy/SchooldeployApplication.java
+++ schooldeploy/schooldeploy/src/main/java/com/lambdaschool/schooldeploy/SchooldeployApplication.java
@@ -2,16 +2,26 @@ package com.lambdaschool.schooldeploy;
 
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
 public class SchooldeployApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SchooldeployApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SchooldeployApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
