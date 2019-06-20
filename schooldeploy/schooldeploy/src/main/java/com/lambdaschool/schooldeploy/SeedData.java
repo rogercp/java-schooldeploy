fd8d207ef911cb1300083cf3835e87a7e1331321
Roger Caballero <rogercp1234@yahoo.com>
2019-06-19 18:05:40 -0700

    commented out seed data and data sql set to never in app properties

--- schooldeploy/schooldeploy/src/main/java/com/lambdaschool/schooldeploy/SeedData.java
+++ schooldeploy/schooldeploy/src/main/java/com/lambdaschool/schooldeploy/SeedData.java
@@ -14,8 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
-@Transactional
-@Component
+//@Transactional
+//@Component
 public class SeedData implements CommandLineRunner
 {
     @Autowired
